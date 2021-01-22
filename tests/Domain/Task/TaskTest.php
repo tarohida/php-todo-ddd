cc3d58998ec9299975ee9a6ab2d6d2576c5051b5
tarohida <sk8trou@gmail.com>
2021-01-22 20:07:05 +0900

    feature/63wnhb Taskクラスの作成（エンティティ）

---
+++ tests/Domain/Task/TaskTest.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace Tests\Domain\Task;
+
+
+use App\Domain\Task\Task;
+use PHPUnit\Framework\TestCase;
+
+class TaskTest extends TestCase
+{
+    private int $id;
+    private string $title;
+    /**
+     * @var Task
+     */
+    private Task $task;
+
+    public function setUp(): void
+    {
+        $this->id = 1;
+        $this->title = 'new task';
+        $this->task = new Task($this->id, $this->title);
+    }
+
+    public function test_construct()
+    {
+        $this->assertInstanceOf(Task::class, $this->task);
+    }
+
+    public function test_property_id_get()
+    {
+        $this->assertSame($this->id, $this->task->id());
+    }
+
+    public function test_property_title_get()
+    {
+        $this->assertSame($this->title, $this->task->title());
+    }
+}
