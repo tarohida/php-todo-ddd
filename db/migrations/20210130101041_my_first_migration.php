a9b037ecdd983716ab3bb0818d84be76a85f4f85
taro-hida <sk8trou@gmail.com>
2021-02-05 18:46:52 +0900

    63wnhb setup phinx

---
+++ db/migrations/20210130101041_my_first_migration.php
@@ -0,0 +1,25 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class MyFirstMigration extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        // create the table
+        $table = $this->table('tasks');
+        $table->addColumn('title', 'text')
+            ->create();
+    }
+}
