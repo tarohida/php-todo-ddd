a8af2b7f1ba6b2e050deadcf96645e5b3abe13a9
taro-hida <sk8trou@gmail.com>
2021-03-13 18:46:50 +0900

    feature/63wnhb Add CreateTaskController class.

---
+++ src/Application/Controller/Http/CreateTaskController.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+
+namespace App\Application\Controller\Http;
+
+
+use App\Application\Actions\Task\CreateTaskAction;
+use App\Domain\Task\Task;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class CreateTaskController implements HttpControllerInterface
+{
+    /**
+     * @var CreateTaskAction
+     */
+    private CreateTaskAction $action;
+
+    public function __construct(CreateTaskAction $action)
+    {
+        $this->action = $action;
+    }
+
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+    {
+        $parameters= $request->getParsedBody();
+        $task_id = (int)$args['id'];
+        $task_title = $parameters['title'];
+        $task = new Task($task_id, $task_title);
+        $this->action->action($task);
+        $response->getBody()->write(sprintf('task created. id: %s, title: %s', $task_id, $task_title));
+        return $response;
+    }
+}
