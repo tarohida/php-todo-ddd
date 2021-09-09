c5062501ff451587f65decbe3a612dee1611cf12
taro-hida <sk8trou@gmail.com>
2021-09-09 21:17:22 +0900

    feature/esa354 \App\Exception\RuntimeException が ApplicationExceptionInterface を継承するように変更

--- src/Exception/RuntimeException.php
+++ src/Exception/RuntimeException.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace App\Exception;
 
-abstract class RuntimeException extends \RuntimeException
+abstract class RuntimeException extends \RuntimeException implements ApplicationExceptionInterface
 {
     abstract public function getDebugPrint() :string;
     protected function getDebugPrintedString(mixed $param): string
