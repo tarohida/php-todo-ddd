d42d51c6ab5e87cd3aebf7b5c45e30f19d88e91a
taro-hida <sk8trou@gmail.com>
2021-09-11 21:29:18 +0900

    feature/esa354 tasks/ への GET リクエストが404を返すテストを追加

---
+++ tests_http/Tasks/GetTasksTest.php
@@ -0,0 +1,29 @@
+<?php
+/** @noinspection NonAsciiCharacters */
+/** @noinspection PhpUnhandledExceptionInspection */
+/** @noinspection PhpDocMissingThrowsInspection */
+/** @noinspection PhpPrivateFieldCanBeLocalVariableInspection */
+/** @noinspection PhpExpressionResultUnusedInspection */
+/** @noinspection PhpStaticAsDynamicMethodCallInspection */
+
+declare(strict_types=1);
+
+namespace HttpTests\Tasks;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use PHPUnit\Framework\TestCase;
+
+class GetTasksTest extends TestCase
+{
+    public function test_get()
+    {
+        $client = new Client();
+        try {
+            $client->request('GET', 'http://web/tasks');
+        } catch (ClientException $e) {
+            $response = $e->getResponse();
+            self::assertSame(404, $response->getStatusCode());
+        }
+    }
+}
