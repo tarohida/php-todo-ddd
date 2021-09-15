8c02c18581ad5b9cc282fd376f56a572dedb6ac7
taro-hida <sk8trou@gmail.com>
2021-09-15 21:01:13 +0900

    feature/esa354 とりあえず Hello, World できるところまで実装

--- public/index.php
+++ public/index.php
@@ -2,7 +2,16 @@
 declare(strict_types=1);
 
 use Slim\Factory\AppFactory;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write('Hello world!');
+    return $response;
+});
+
+$app->run();
