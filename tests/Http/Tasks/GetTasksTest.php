cfffe4bd8316f3ef29edb292aa0170775b862b51
taro-hida <sk8trou@gmail.com>
2021-09-19 21:05:03 +0900

    feature/esa354 tests_http が存在すると IDE でのテスト作成のショートカットがうまく動作しないため、テストコードは tests 以下の名前空間にすべて設置することにする

--- tests/Http/Tasks/GetTasksTest.php
+++ tests/Http/Tasks/GetTasksTest.php
@@ -1,18 +1,20 @@
-<?php
+<?php 
 /** @noinspection NonAsciiCharacters */
 /** @noinspection PhpUnhandledExceptionInspection */
 /** @noinspection PhpDocMissingThrowsInspection */
 /** @noinspection PhpPrivateFieldCanBeLocalVariableInspection */
 /** @noinspection PhpExpressionResultUnusedInspection */
 /** @noinspection PhpStaticAsDynamicMethodCallInspection */
+/** @noinspection HttpUrlsUsage */
 
 declare(strict_types=1);
 
-namespace HttpTests\Tasks;
+namespace Tests\Http\Tasks;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ClientException;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
 
 class GetTasksTest extends TestCase
 {
@@ -22,7 +24,7 @@ class GetTasksTest extends TestCase
         self::assertSame(200, $response->getStatusCode());
     }
 
-    private function getResponse()
+    private function getResponse(): ResponseInterface
     {
         $client = new Client();
         try {
