2924c5fc9f93210a05d095ebca3fdac028b48686
taro-hida <sk8trou@gmail.com>
2021-09-22 21:00:39 +0900

    feature/esa354 /list のコントローラを作成

--- src/Application/Http/Controller/ListTaskController.php
+++ src/Application/Http/Controller/ListTaskController.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 
 namespace App\Application\Http\Controller;
 
+use App\Application\Http\Controller\Exception\JsonConvertFailedException;
 use App\Domain\Task\ListTaskService;
+use App\Domain\Task\TaskList;
 use Slim\Psr7\Request;
 use Slim\Psr7\Response;
 
@@ -11,11 +13,27 @@ class ListTaskController implements SlimHttpControllerInterface
 {
     public function __invoke(Request $request, Response $response, $args): Response
     {
-        $response->getBody()->write('Hello world!');
+        $response->getBody()->write($this->dumpJson($this->service->list()));
         return $response;
     }
 
     public function __construct(
         private ListTaskService $service
     ) { }
+
+    private function dumpJson(TaskList $tasks): bool|string
+    {
+        $raw_result = [];
+        foreach ($tasks as $task) {
+            $result[] = [
+                'id' => $task->id(),
+                'title' => $task->title()
+            ];
+        }
+        $result = json_encode($raw_result);
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new JsonConvertFailedException(params: $raw_result);
+        }
+        return $result;
+    }
 }
